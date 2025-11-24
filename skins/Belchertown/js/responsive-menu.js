d0c4d02d285f04aac15ef91dfc545541ba9431b2
uajqq <uajqq@proton.me>
2025-11-23 19:59:43 -0500

    Code cleanup, formatting

--- skins/Belchertown/js/responsive-menu.js
+++ skins/Belchertown/js/responsive-menu.js
@@ -1,22 +1,22 @@
-jQuery(function( $ ){
+jQuery(function($) {
 
 	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
 
-	$(".responsive-menu-icon").click(function(){
+	$(".responsive-menu-icon").click(function() {
 		$(this).next("header .genesis-nav-menu, .nav-primary .genesis-nav-menu").slideToggle();
 	});
 
-	$(window).resize(function(){
-		if(window.innerWidth > 768) {
+	$(window).resize(function() {
+		if (window.innerWidth > 768) {
 			$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, nav .sub-menu").removeAttr("style");
 			$(".responsive-menu > .menu-item").removeClass("menu-open");
 		}
 	});
 
-	$(".responsive-menu > .menu-item").click(function(event){
+	$(".responsive-menu > .menu-item").click(function(event) {
 		if (event.target !== this)
-		return;
-			$(this).find(".sub-menu:first").slideToggle(function() {
+			return;
+		$(this).find(".sub-menu:first").slideToggle(function() {
 			$(this).parent().toggleClass("menu-open");
 		});
 	});
