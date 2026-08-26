3a35bb3f1142dc3a18696977d6c9b555cf482c89
uajqq <uajqq@proton.me>
2026-08-26 10:14:52 -0400

    Fix outdated Facebook widget

--- bin/user/new_belchertown.py
+++ bin/user/new_belchertown.py
@@ -7842,7 +7842,7 @@ class getData(SearchList):
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) return;
                 js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+                js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
                 fjs.parentNode.insertBefore(js, fjs);
                 }(document, 'script', 'facebook-jssdk'));</script>
                 <div class="fb-like" data-href="%s" data-width="500px" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
